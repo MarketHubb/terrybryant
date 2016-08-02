871f3c3158e4a4f68b18f9239d21da76ba23b3ec
Chris Hubbard <chris@markethubb.com>
2016-08-02 11:57:18 -0400

    Customize menu item display in backend

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -21,9 +21,11 @@ if (!is_admin()) {
     wp_enqueue_script('font-awesome-scripts');
     wp_enqueue_script('bootstrap-scripts');
     wp_enqueue_script('practice-scripts');
-    wp_enqueue_script('apex-chat');
 }
 
+if (!is_admin() && $GLOBALS['pagenow'] !== 'wp-login.php') {
+    wp_enqueue_script('apex-chat');
+}
 /**
  * ACF Global Options
  */
@@ -36,3 +38,62 @@ if( function_exists('acf_add_options_page') ) {
         'redirect'		=> false
     ));
 }
+
+/**
+ * Remove default menu items in WP admin
+ */
+add_action( 'admin_menu', 'mh_remove_menus', 999 );
+function mh_remove_menus()
+{
+    global $menu;
+    global $submenu;
+
+    // Menu pages
+    remove_menu_page( 'edit.php' );                   //Posts
+    remove_menu_page( 'edit.php?post_type=page' );    //Pages
+    remove_menu_page( 'index.php' );                  //Dashboard
+    remove_menu_page( 'edit-comments.php' );          //Comments
+    remove_menu_page( 'themes.php' );                 //Appearance
+    remove_menu_page( 'plugins.php' );                //Plugins
+    /*remove_menu_page( 'tools.php' );*/                  //Tools
+    remove_menu_page( 'options-general.php' );        //Settings
+    remove_menu_page( 'cptui_main_menu' );            //Custom Post Type UI
+    remove_menu_page( 'edit.php?post_type=acf-field-group' ); //ACF
+    remove_menu_page( 'admin.php?page=wpseo_dashboard' ); //Yoast
+
+    // Submenu pages
+    remove_submenu_page( 'index.php', 'update-core.php' );
+    remove_submenu_page( 'gf_edit_forms', 'gf_new_form' );
+    remove_submenu_page( 'gf_edit_forms', 'gf_update' );
+    remove_submenu_page( 'gf_edit_forms', 'gf_settings' );
+    remove_submenu_page( 'gf_edit_forms', 'gf_help' );
+    remove_submenu_page( 'gf_edit_forms', 'gf_export' );
+    remove_submenu_page( 'gf_edit_forms', 'gf_addons' );
+
+}
+
+/**
+ * Remove default WP footer text
+ */
+add_filter('admin_footer_text', mh_remove_admin_footer_text, 1000);
+function mh_remove_admin_footer_text($footer_text =''){
+    return '';
+}
+
+add_filter('update_footer', mh_remove_admin_footer_upgrade, 1000);
+function mh_remove_admin_footer_upgrade($footer_text =''){
+    return '';
+}
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        body {
+            background: #eee !important;
+        }
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/Terry-Bryant-Logo.svg);
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
