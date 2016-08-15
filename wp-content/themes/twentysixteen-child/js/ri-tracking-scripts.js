33a274e987833f2c628b8ae41b4c81fca1c03178
Chris Hubbard <chris@markethubb.com>
2016-08-15 10:27:24 -0400

    Update Google Analytics event tracking script for ApexChat

--- wp-content/themes/twentysixteen-child/js/ri-tracking-scripts.js
+++ wp-content/themes/twentysixteen-child/js/ri-tracking-scripts.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 	// Google Analytics Event Tracking: ApexChat
 	window.ChatInterceptor = function () {
-		ga('send', 'event', 'ApexChat', 'Chat', 'Lead', 0);
+		__gaTracker('send', 'event', 'ApexChat', 'Chat', 'Lead', 1);
     };
 });
