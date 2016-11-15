ac918b499e52f58af62be213c4279fba812e1abc
Chris Hubbard <chris@markethubb.com>
2016-11-15 17:14:53 -0500

    Insert Google Tag Manager tracking script

--- wp-content/themes/twentysixteen-child/header.php
+++ wp-content/themes/twentysixteen-child/header.php
@@ -24,13 +24,13 @@
 <body <?php body_class(); ?>>
 
 <!-- Google Tag Manager -->
-<!-- <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T7W5GP"
+ <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T7W5GP"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T7W5GP');</script> -->
+})(window,document,'script','dataLayer','GTM-T7W5GP');</script>
 <!-- End Google Tag Manager -->
 
 <!-- Google Analytics Tracking Script -->
@@ -52,4 +52,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 </script>
 
 <!-- Bing Ads UET Tracking Script -->
-<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"5317830"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script><noscript><img src="//bat.bing.com/action/0?ti=5317830&Ver=2" height="0" width="0" style="display:none; visibility: hidden;" /></noscript>
+<!--<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"5317830"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script><noscript><img src="//bat.bing.com/action/0?ti=5317830&Ver=2" height="0" width="0" style="display:none; visibility: hidden;" /></noscript>-->
